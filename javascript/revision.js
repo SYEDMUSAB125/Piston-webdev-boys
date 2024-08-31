47c56d9bf65993245a80ccbf0171c9d4f29be18e
Syed Musab <syedmasab125@gmail.com>
2024-08-30 23:15:56 -0700

    revision class

---
+++ javascript/revision.js
@@ -0,0 +1,97 @@
+// data type 
+// primitive copy value to value
+// string "34 " , number , boolean   0 1   
+// var a num = num + 1 
+// num2
+// var b = 1
+// console.log(a)
+// console.log(a)
+//  var result = b + a 
+//  console.log(result)
+0 
+// num = num + 1 
+// num++ post increment
+// console.log(num )
+// num--post decrement 
+// --num pre decrement 
+// ++num pre  
+
+// const constant 
+
+
+// var a = 0
+// console.log(a)
+// a++
+// console.log(a)
+// var b = 0
+// ++b
+// console.log(b) 
+
+// for(let i = 0 ; i< 10  ; ++i){
+//     console.log(i)
+// }
+// let i = 0
+// ++i
+// while(i<10){
+//     console.log(i)
+//     i++
+// }
+// let i =1 
+// do{
+// console.log(i)
+// }while(i === 0 )
+
+
+
+// array 
+// const cities = [ "karachi","lahore", "islamabad" ]
+// for of array   
+// for(const element of cities   ){
+//     console.log(element)
+// }
+
+
+// for( let i = 0 ; i < cities.length; i++ ){
+//     console.log(cities[i])
+// } 
+
+const obj = {
+ name : "Musab",
+ age: " 34",
+ height : "5.4 "
+
+}
+// for in loop  obj
+for(const key in obj ){
+    console.log(obj[key])
+}
+
+
+// destructure
+// const {name } = obj 
+// console.log(name);
+
+// console.log(obj.name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
